979a036544c246c2c11b690fae4f3b4921429f93
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-04-04 17:16:08 -0600

    first prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword () {
+  var psswrdLength = prompt("How many characters would you like your password to be? (please choose a number between 8 to 128)");
+  if (generatePassword < 8 || generatePassword > 128)
+  return false;
+  alert ("Invalid, please try again.");
+  if (generatePassword > 8 || generatePassword < 128)
+  return 
+}
 
 // Write password to the #password input
 function writePassword() {
