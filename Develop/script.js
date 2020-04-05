398236a326890add1a57997d64d508dae6258881
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-04-05 15:21:08 -0600

    added array

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,95 @@
-function generatePassword () {
-  var promptLength = Number(prompt("How many characters would you like your password to be? (please choose a number between 8 to 128)"));
-  if (isNaN(promptLength) || promptLength < 8 || promptLength > 128) 
-  promptLength = Number (prompt("Invalid, please try again."));
+var symbolsArr = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowercaseArr = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var uppercaseArr = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]
+
+function generatePassword() {
+  var passwordLength = Number(prompt("How many characters would you like your password to be? (please choose a number between 8 to 128)"));
+
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) 
+  passwordLength = Number (prompt("Invalid, please try again."));
 
   var lowercase = confirm("Do you want lowercase characters?");
   var uppercase = confirm("Do you want uppercase characters?");
@@ -15,32 +103,93 @@ function generatePassword () {
     numbers = confirm("Do you want numbers?");
     symbols = confirm("Do you want special characters?");
   }
-
-  function generatePassword (length = promptLength) {
-    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var numbers = '0123456789';
-    var symbols = '!@#$%^&*(){}[]=<>/,.';
-
-    var all = uppercase + lowercase + numbers + symbols;
-
-    var password = '';
-
-    for (i = o; i < promptLength; i ++) {
-      result += characters.charAt(Math.floor(Math.random() * promptLength));
-    }
-    
-    return password;
-  }
+  var passwordOptions = {
+    length: passwordLength,
+    hasSpecialCharacters: symbols,
+    hasNumericCharacters: numbers,
+    hasLowerCasedCharacters: lowercase,
+    hasUpperCasedCharacters: uppercase, 
+  };
+  return passwordOptions;
 }
+function generate() {
+    var passwordOptions=generatePassword();
+    var passwordChar=[];
+    var lowercaseChar=[];
+    if (passwordOptions.hasLowerCasedCharacters) {
+      for (var i=0; i < passwordOptions.length; i++) {
+        var lowercaseChar = lowercaseArr[Math.floor(Math.random() * passwordOptions.length)];
+        passwordChar.push(lowercaseChar[i]);
+        console.log(lowercaseChar[i]);
+      }
+    }
+    var uppercaseChar=[];
+    if (passwordOptions.hasUpperCasedCharacters) {
+      for (var i=0; i < passwordOptions.length; i++) {
+        var uppercaseChar = uppercaseArr[Math.floor(Math.random() * passwordOptions.length)];
+        passwordChar.push(uppercaseChar[i]);
+        console.log(uppercaseChar[i]);
+    }
+    var numbersChar=[];
+    if (passwordOptions.hasNumericCharacters) {
+      for (var i=0; i < passwordOptions.length; i++) {
+        var numbersChar = numbersArr[Math.floor(Math.random() * passwordOptions.length)];
+        passwordChar.push(numbersChar[i]);
+        console.log(numbersChar[i]);
+    }
+    var symbolsChar=[];
+    if (passwordOptions.hasSpecialCharacters) {
+      for (var i=0; i < passwordOptions.length; i++) {
+        var symbolsChar = symbolsArr[Math.floor(Math.random() * passwordOptions.length)];
+        passwordChar.push(symbolsChar[i]);
+        console.log(symbolsChar[i]);
+    }
+    var finalChar=passwordChar[Math.floor(Math.random() * passwordOptions.length)];
+      console.log(finalChar);
+    }
+  }
 
+  // function generate () {
+  //   userPassword = "";
+  //   passwordCharSet = "";
+  //   if (lowercaseInput) {
+  //     passwordCharSet += lowercase;
+  //   }
+  //   if (uppercaseInput) {
+  //     passwordCharSet += uppercase;
+  //   }
+  //   if (numbersInput) {
+  //     passwordCharSet += numbers;
+  //   }
+  //   if (symbolsInput) {
+  //     passwordCharSet += symbols;
+  //   }
+
+  //   passwordLength = Number(passwordLengthInput);
+
+  //   var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  //   var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  //   var numbers = '0123456789';
+  //   var symbols = '!@#$%^&*(){}[]=<>/,.';
+  //   lowercaseInput = document.getElementById("lowercase");
+  //   uppercaseInput = document.getElementById("uppercase");
+  //   numbersInput = document.getElementById("numbers");
+  //   symbolsInput = document.getElementById("symbols");
+  //   passwordLengthInput = document.getElementById("passwordLength")
+
+  //   for (i = 0; i < passwordLength; i ++) {
+  //     userPassword += passwordCharSet.charAt(Math.floor(Math.random() * passwordCharSet.passwordLength));
+  //   }
+
+  //   return password;
+  // }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generate();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -49,3 +198,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
+}
