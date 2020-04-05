81ecc361c2b7a6c400d228c0886cbde8c5e85b68
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-04-05 01:01:48 -0600

    added prompts for characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,42 @@
+function generatePassword () {
+  var promptLength = Number(prompt("How many characters would you like your password to be? (please choose a number between 8 to 128)"));
+  if (isNaN(promptLength) || promptLength < 8 || promptLength > 128) 
+  promptLength = Number (prompt("Invalid, please try again."));
+
+  var lowercase = confirm("Do you want lowercase characters?");
+  var uppercase = confirm("Do you want uppercase characters?");
+  var numbers = confirm("Do you want numbers?");
+  var symbols = confirm("Do you want special characters?");
+
+  if (!(lowercase && uppercase && numbers && symbols)) {
+    alert ("Please pick at least one character type.");
+    lowercase = confirm("Do you want lowercase characters?");
+    uppercase = confirm("Do you want uppercase characters?");
+    numbers = confirm("Do you want numbers?");
+    symbols = confirm("Do you want special characters?");
+  }
+
+  function generatePassword (length = promptLength) {
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numbers = '0123456789';
+    var symbols = '!@#$%^&*(){}[]=<>/,.';
+
+    var all = uppercase + lowercase + numbers + symbols;
+
+    var password = '';
+
+    for (i = o; i < promptLength; i ++) {
+      result += characters.charAt(Math.floor(Math.random() * promptLength));
+    }
+    
+    return password;
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-function generatePassword () {
-  var psswrdLength = prompt("How many characters would you like your password to be? (please choose a number between 8 to 128)");
-  if (generatePassword < 8 || generatePassword > 128)
-  return false;
-  alert ("Invalid, please try again.");
-  if (generatePassword > 8 || generatePassword < 128)
-  return 
-}
+
 
 // Write password to the #password input
 function writePassword() {
